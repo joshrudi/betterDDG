1307ee8c58c0a5f1614678d12da6e3f9ccf07e13
Joshua Rudaitis <jrudaitis@outlook.com>
2019-11-27 03:25:32 -0500

    favicon support

--- src/betterduck.js
+++ src/betterduck.js
@@ -4,3 +4,7 @@ if (document.getElementsByClassName("header__logo").length !== 0) {
 if (document.getElementsByClassName("logo_homepage").length !== 0) {
     document.getElementsByClassName("logo_homepage")[1].style = `background-image: url("https://raw.githubusercontent.com/joshrudi/betterDDG/master/src/icons/duck.png")`;
 }
+
+var tabIcon = document.querySelector('link[rel="shortcut icon"]');
+tabIcon.setAttribute('type', 'image/png');
+tabIcon.setAttribute('href', "https://raw.githubusercontent.com/joshrudi/betterDDG/master/src/icons/duck.png");
