9812b65f78540d2d2482fa8126e0dead7fba7347
Jared Taylor <jared.xyz@mailbox.org>
2019-11-27 03:17:23 -0500

    Search page working

--- src/betterduck.js
+++ src/betterduck.js
@@ -1,2 +1,6 @@
-document.body.style.border = "5px solid red";
-document.getElementsByClassName("logo_homepage")[1].style = `background-image: url("https://raw.githubusercontent.com/joshrudi/betterDDG/master/src/icons/duck.png")`;
+if (document.getElementsByClassName("header__logo").length !== 0) {
+    document.getElementsByClassName("header__logo")[0].style = `background-image: url("https://raw.githubusercontent.com/joshrudi/betterDDG/master/src/icons/duck.png")`;
+}
+if (document.getElementsByClassName("logo_homepage").length !== 0) {
+    document.getElementsByClassName("logo_homepage")[1].style = `background-image: url("https://raw.githubusercontent.com/joshrudi/betterDDG/master/src/icons/duck.png")`;
+}
